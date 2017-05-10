1a788ac68cff3c2fb36c3bb9c7e96a7d5a7b3a47
Faheem Akhtar <faheempiscean@gmail.com>
2017-05-10 17:17:00 +0400

    added a simple express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
