d9c4073122240c03d1ab091b91114bb30782ad0d
Faheem Akhtar <faheempiscean@gmail.com>
2017-05-11 17:32:07 +0400

    REDRO-109 added express for SSR

--- server/index.js
+++ server/index.js
@@ -1,14 +1,43 @@
 const express = require('express')
+const Vue = require('vue')
+const fs = require('fs')
 const path = require('path')
+
+const renderer = require('vue-server-renderer').createRenderer({
+  template: fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf-8')
+})
 
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, '../dist')));
+// app.use(express.static(path.resolve(__dirname, '../dist')));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+app.get('*', (req, res) => {
+  const vueApp = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`
+  })
+  renderer.renderToString(vueApp, (error, html) => {
+    if(error) {
+      res.status(500).end(error)
+      return
+    }
+    res.send(html)
+    // res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+  })
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
+
+// const Vue = require('vue')
+
+
+
+
+// renderer.renderToString(app, (err, html) => {
+//   if (err) throw err
+//   console.log(html)
+// })
